85adf51e3ea9f014bac38e443724dc690985ae5c
Paavo <pparkkin@gmail.com>
2017-10-18 13:10:11 +0800

    Collect some table data and store as batch.

--- DBLoad.hs
+++ DBLoad.hs
@@ -28,6 +28,7 @@ import Database.SQLite.Simple
 -- table and pull into normalized tables from there using SQL. Problem
 -- is the large number of columns from the questions.
 
+-- StateT stuff
 type IdCache = Map T.Text Int
 type IdCaches = Map T.Text IdCache
 
@@ -38,19 +39,50 @@ cacheId t (v, i) = M.alter alterTable t
     alterTable Nothing = Just $ M.singleton v i
     alterId _ = Just i
 
+type TableQuery = T.Text
+type TableRow = [SQLData]
+data TableData = TableData TableQuery [TableRow]
+type TableName = T.Text
+type TableDataMap = Map TableName TableData
+
+appendTableData :: TableName -> TableQuery -> [TableRow] -> TableDataMap -> TableDataMap
+appendTableData n q rs m =
+  case M.lookup n m of
+    Nothing -> M.insert n (TableData q rs) m
+    Just (TableData q' rs') -> M.insert n (TableData q' (rs ++ rs')) m
+
+type ETLState = (IdCaches, TableDataMap)
+
+
+
 loadData :: (Vector B.ByteString, Vector (Vector B.ByteString)) -> Connection -> IO ()
 loadData durtur conn = do
-  evalStateT (loadData' durtur conn) M.empty
+  evalStateT (loadData' durtur conn) (M.empty, M.empty)
 
-loadData' :: (Vector B.ByteString, Vector (Vector B.ByteString)) -> Connection -> StateT IdCaches IO ()
+loadData' :: (Vector B.ByteString, Vector (Vector B.ByteString)) -> Connection -> StateT ETLState IO ()
 loadData' (headers, csvData) conn = do
   lift $ putStrLn "Loading data into database."
   mapM_ (\(t, f) -> loadCollectionTable conn t (f csvData)) collectionTables
   ks <- loadKysymyksetTable conn headers
   loadVastaajatTable conn ks csvData
+  (_, tm) <- S.get
+  let sts = manyToManyTables ++
+            [ "vastaajat"
+            , "vastaaja_vastaukset"
+            ]
+  mapM_ (storeTable tm) sts
   lift $ putStrLn "All done."
+  where
+    -- storeTable :: TableDataMap -> TableName -> StateT ETLState IO ()
+    storeTable tm tn = do
+      case M.lookup tn tm of
+        Nothing -> do
+          lift $ putStrLn $ "Could not find data for table " ++ (T.unpack tn) ++ "."
+        Just (TableData q vs) -> do
+          lift $ putStrLn $ "Storing data for table " ++ (T.unpack tn) ++ "."
+          lift $ executeMany conn (Query q) vs
 
-loadKysymyksetTable :: Connection -> Vector B.ByteString -> StateT IdCaches IO [(Int, (Int, T.Text))]
+loadKysymyksetTable :: Connection -> Vector B.ByteString -> StateT ETLState IO [(Int, (Int, T.Text))]
 loadKysymyksetTable conn headers = do
   lift $ putStrLn "Loading table kysymykset."
   lift $ executeMany conn q vs
@@ -60,25 +92,30 @@ loadKysymyksetTable conn headers = do
     vs = map (\(i, (_, k)) -> (i, k)) ks
     q = "INSERT INTO kysymykset (id, kysymys) VALUES (?, ?)"
 
-loadVastaajatTable :: Connection -> [(Int, (Int, T.Text))] -> Vector (Vector B.ByteString) -> StateT IdCaches IO ()
+loadVastaajatTable :: Connection -> [(Int, (Int, T.Text))] -> Vector (Vector B.ByteString) -> StateT ETLState IO ()
 loadVastaajatTable conn ks csvData = do
   lift $ putStrLn "Loading table vastaajat."
   let fs = intercalate "," fieldNames
       qms = intercalate "," $ map (const "?") fieldNames
-      q = Query $ T.pack $ "INSERT INTO vastaajat (" ++ fs ++ ") VALUES (" ++ qms ++ ")"
-      qv = Query $ "INSERT INTO vastaaja_vastaukset (vastaaja_id, kysymys_id, vastaus_id) VALUES (?, ?, ?)"
+      q = T.pack $ "INSERT INTO vastaajat (" ++ fs ++ ") VALUES (" ++ qms ++ ")"
+      qv = "INSERT INTO vastaaja_vastaukset (vastaaja_id, kysymys_id, vastaus_id) VALUES (?, ?, ?)"
   -- TODO: This is getting pretty ugly. Should use the StateT instead, but too lazy right now.
   (ps, vs) <- V.foldM collectRows ([],[]) csvData
-  lift $ putStrLn ""
-  lift $ executeMany conn q ps
-  lift $ executeMany conn qv vs
+  (ic, tm) <- S.get
+  let tm' = M.fromList [ ("vastaajat", (TableData q ps))
+                       , ("vastaaja_vastaukset", (TableData qv vs))
+                       ]
+  S.put (ic, M.union tm' tm)
+  lift $ putStrLn "Done"
+  -- lift $ executeMany conn (Query q) ps
+  -- lift $ executeMany conn qv vs
   where
-    collectRows :: ([[SQLData]], [(Int, Int, Int)]) -> Vector B.ByteString -> StateT IdCaches IO ([[SQLData]], [(Int, Int, Int)])
+    collectRows :: ([[SQLData]], [[SQLData]]) -> Vector B.ByteString -> StateT ETLState IO ([[SQLData]], [[SQLData]])
     collectRows (ps, vs) row = do
       (p, v) <- loadVastaajatRow conn ks row
       return (p:ps, v++vs)
 
-loadVastaajatRow :: Connection -> [(Int, (Int, T.Text))] -> Vector B.ByteString -> StateT IdCaches IO ([SQLData], [(Int, Int, Int)])
+loadVastaajatRow :: Connection -> [(Int, (Int, T.Text))] -> Vector B.ByteString -> StateT ETLState IO ([SQLData], [[SQLData]])
 loadVastaajatRow conn ks row = do
   params <- sequence $ constructVastaajaParams conn row fieldMapping
   let ("id", (SQLInteger vid')) = head params
@@ -86,14 +123,19 @@ loadVastaajatRow conn ks row = do
   mapM_ (loadManyToMany conn row vid) manyToManyMapping
   vs <- loadVastaajatVastaukset conn ks vid row
   lift $ putStr "."
-  return $ (map snd params, vs)
+  return $ (map snd params, map toSQLDataList vs)
+  where
+    toSQLDataList (i1, i2, i3) = [ SQLInteger (fromIntegral i1)
+                                 , SQLInteger (fromIntegral i2)
+                                 , SQLInteger (fromIntegral i3)
+                                 ]
 
-loadVastaajatVastaukset :: Connection -> [(Int, (Int, T.Text))] -> Int -> Vector B.ByteString -> StateT IdCaches IO [(Int, Int, Int)]
+loadVastaajatVastaukset :: Connection -> [(Int, (Int, T.Text))] -> Int -> Vector B.ByteString -> StateT ETLState IO [(Int, Int, Int)]
 loadVastaajatVastaukset conn ks vid row = do
   vs <- mapM (loadVastaajatVastaus conn vid row) ks
   return (catMaybes vs)
 
-loadVastaajatVastaus :: Connection -> Int -> Vector B.ByteString -> (Int, (Int, T.Text)) -> StateT IdCaches IO (Maybe (Int, Int, Int))
+loadVastaajatVastaus :: Connection -> Int -> Vector B.ByteString -> (Int, (Int, T.Text)) -> StateT ETLState IO (Maybe (Int, Int, Int))
 loadVastaajatVastaus conn vid row (ki, (kc, _)) = do
   let v = textColumnValue kc row
       k = textColumnValue (kc + 1) row
@@ -110,13 +152,18 @@ pairs [] = []
 pairs [x] = []
 pairs (x:y:xs) = (x, y) : pairs xs
 
-loadManyToMany :: Connection -> Vector B.ByteString -> Int -> ManyToMany -> StateT IdCaches IO ()
+loadManyToMany :: Connection -> Vector B.ByteString -> Int -> ManyToMany -> StateT ETLState IO ()
 loadManyToMany conn row vid (ManyToMany jt cn c vt) = do
   let vals = V.toList $ uniques $ parseMultiple $ textColumnValue c row
-      q = Query $ "INSERT INTO " `T.append` jt `T.append` " (vastaaja_id, " `T.append` cn `T.append` ") values (?, ?)"
+      q = "INSERT INTO " `T.append` jt `T.append` " (vastaaja_id, " `T.append` cn `T.append` ") values (?, ?)"
   ids <- queryIds conn vt vals
   ps <- lift $ mapM (loadManyToManyEntry jt vid) (zip vals ids)
-  lift $ executeMany conn q (catMaybes ps)
+  let vs = map toSQLDataList (catMaybes ps)
+  (ic, tm) <- S.get
+  S.put (ic, appendTableData jt q vs tm)
+  -- lift $ executeMany conn (Query q) (catMaybes ps)
+  where
+    toSQLDataList (i1, i2) = [SQLInteger (fromIntegral i1), SQLInteger (fromIntegral i2)]
 
 loadManyToManyEntry :: T.Text -> Int -> (T.Text, Maybe Int) -> IO (Maybe (Int, Int))
 loadManyToManyEntry _ vid (_, (Just cid)) = do
@@ -125,13 +172,13 @@ loadManyToManyEntry t _ (v, Nothing) = do
   putStrLn $ "Could not find ID for value " ++ (T.unpack v) ++ " for table " ++ (T.unpack t)
   return Nothing
 
-constructVastaajaParams :: Connection -> Vector B.ByteString -> [Maybe (String, FieldMapping)] -> [StateT IdCaches IO (String, SQLData)]
+constructVastaajaParams :: Connection -> Vector B.ByteString -> [Maybe (String, FieldMapping)] -> [StateT ETLState IO (String, SQLData)]
 constructVastaajaParams conn row fms = foldr f [] (zip (V.toList row) fms)
   where
     f (_, Nothing) ps = ps
     f (b, Just fm) ps = fmap (\d -> (fst fm, d)) (constructVastaajaParam conn b (snd fm)) : ps
 
-constructVastaajaParam :: Connection -> B.ByteString -> FieldMapping -> StateT IdCaches IO SQLData
+constructVastaajaParam :: Connection -> B.ByteString -> FieldMapping -> StateT ETLState IO SQLData
 constructVastaajaParam _ b (IntColumnIndex n f) = do
   let cv = textValue b
       iv = f cv
@@ -154,12 +201,12 @@ constructVastaajaParam c b (TableReference n t) = do
       lift $ putStrLn $ "Unable to find value '" ++ (T.unpack cv) ++ "' in table '" ++ (T.unpack t) ++ "'"
       return (SQLInteger (-1))
 
-loadCollectionTable :: Connection -> String -> Vector (Int, T.Text) -> StateT IdCaches IO ()
+loadCollectionTable :: Connection -> String -> Vector (Int, T.Text) -> StateT ETLState IO ()
 loadCollectionTable c n d = do
   lift $ putStrLn ("Loading table " ++ n ++ ".")
   lift $ executeMany c q vs
-  s <- S.get
-  S.put $ foldr (\v m -> cacheId (T.pack n) (swap v) m) s vs
+  (s, t) <- S.get
+  S.put $ (foldr (\v m -> cacheId (T.pack n) (swap v) m) s vs, t)
   where
     q = Query $ T.pack $ "INSERT INTO " ++ n ++ " (id, value) VALUES (?, ?)"
     vs = V.toList d
@@ -239,12 +286,12 @@ parseMultiple = V.fromList . splitValues
 splitValues :: T.Text -> [T.Text]
 splitValues = (map T.strip) . (T.splitOn "|")
 
-queryIds :: Connection -> T.Text -> [T.Text] -> StateT IdCaches IO [Maybe Int]
+queryIds :: Connection -> T.Text -> [T.Text] -> StateT ETLState IO [Maybe Int]
 queryIds conn t = mapM (queryId conn t)
 
-queryId :: Connection -> T.Text -> T.Text -> StateT IdCaches IO (Maybe Int)
+queryId :: Connection -> T.Text -> T.Text -> StateT ETLState IO (Maybe Int)
 queryId c t cv = do
-  c <- S.get
+  (c, _) <- S.get
   case M.lookup t c of
     Just m ->
       return (M.lookup cv m)
@@ -361,10 +408,15 @@ fieldNames = [ "id"
              , "sijoitukset"
              ]
 
-data ManyToMany = ManyToMany T.Text T.Text Int T.Text
+data ManyToMany = ManyToMany TableName T.Text Int T.Text
                 -- "join_table_name" "join_table_column_name" <csv_column> "value_table"
 
 manyToManyMapping :: [ManyToMany]
 manyToManyMapping = [ ManyToMany "kielitaidot" "kieli_id" 29 "kielet"
                     , ManyToMany "poliittiset_kokemukset" "kokemus_id" 32 "kokemukset"
                     ]
+
+manyToManyTables :: [TableName]
+manyToManyTables = map mtmt manyToManyMapping
+  where
+    mtmt (ManyToMany t _ _ _) = t
